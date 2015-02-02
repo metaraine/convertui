05044e3db7286d4234c702b09ff2218af51bafeb
metaraine <raineorshine@gmail.com>
2015-02-02 11:30:58 -0700

    Add reset button. Add expanding/sliding output box on upload.

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -1,16 +1,36 @@
 (function() {
   $(function() {
+    var output, outputOuterHeight;
+    output = $('#output');
+    outputOuterHeight = output.height() + parseInt(output.css('padding-top')) * 2;
     $("#fine-uploader").fineUploader({
       request: {
         endpoint: "/upload"
       }
+    }).on('submitted', function(id, name) {
+      var inputHeight;
+      inputHeight = $('#input-and-convert').height();
+      console.log(inputHeight);
+      $('#input-and-convert').slideUp();
+      return $('#output').animate({
+        height: outputOuterHeight + inputHeight
+      });
     }).on('complete', function(event, id, name, response) {
       return $('#output').val(response.output);
     });
-    return $('#form-convert').on('submit', function() {
+    $('#form-convert').on('submit', function() {
       $.post('/convert', $(this).serialize(), function(data) {
         return $('#output').val(data);
       });
+      return false;
+    });
+    return $('#reset').on('click', function() {
+      $('#input-and-convert').slideDown();
+      $('#output').animate({
+        height: outputOuterHeight
+      });
+      $('#input').val('');
+      $('#output').val('');
       return false;
     });
   });
--- src/public/scripts/index.coffee
+++ src/public/scripts/index.coffee
@@ -1,11 +1,30 @@
 $ ->
+	output = $('#output')
+	outputOuterHeight = output.height() + parseInt(output.css('padding-top')) * 2
+
 	$("#fine-uploader").fineUploader
 		request:
 			endpoint: "/upload"
+
+	.on 'submitted', (id, name)->
+		inputHeight = $('#input-and-convert').height()
+		console.log inputHeight
+		$('#input-and-convert').slideUp()
+		$('#output').animate
+			height: outputOuterHeight + inputHeight
+
 	.on 'complete', (event, id, name, response)->
 		$('#output').val(response.output)
 
 	$('#form-convert').on 'submit', ()->
 		$.post '/convert', $(this).serialize(), (data)->
 			$('#output').val(data)
+		false
+
+	$('#reset').on 'click', ()->
+		$('#input-and-convert').slideDown()
+		$('#output').animate
+			height: outputOuterHeight
+		$('#input').val('')
+		$('#output').val('')
 		false
